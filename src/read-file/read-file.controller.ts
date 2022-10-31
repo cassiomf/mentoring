08098132822c52ad6f9f5dfbc1f7df9688af0e5b
Cassio Fernandes <cassio.fernandes@sap.com>
2022-10-31 16:08:25 -0300

    creating payload to receive file

--- src/read-file/read-file.controller.ts
+++ src/read-file/read-file.controller.ts
@@ -1,27 +1,52 @@
-import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  ParseFilePipe,
+  ParseFilePipeBuilder,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('read-file')
 export class ReadFileController {
-
   @Get('testing')
   @ApiResponse({
-  status: 200,
-  description: 'Just a test',
+    status: 200,
+    description: 'Just a test',
   })
   testing(): string {
-    return "OK";
+    return 'OK';
   }
 
-  @Post('upload-file')
+  @Post('file')
   @UseInterceptors(FileInterceptor('file'))
   @ApiResponse({
     status: 201,
     description: 'Uploads and process a csv file',
   })
-  processFile(@UploadedFile() file: Express.Multer.File): string {
-    console.log(file);
-    return "OK";
+  uploadFileAndPassValidation(
+    @Body() body: any,
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'csv',
+        })
+        .addMaxSizeValidator({
+          maxSize: 1024,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return {
+      file: file.buffer.toString(),
+    };
   }
 }
